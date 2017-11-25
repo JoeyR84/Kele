d4db1df936aff39a7ae7bf6c4a998ef99c5b3ce9
Joey R <joeyr84@gmail.com>
2017-11-24 20:56:20 -0700

    retrieve complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -17,6 +18,11 @@ class Kele
 
     response = self.class.post('/sessions', options)
     puts response
-    @auth_token = response.body["auth_token"]
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    @user    = JSON.parse(response.body)
   end
 end
